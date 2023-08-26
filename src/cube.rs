8880c0afa6b708dda1e711239da49041c59b9242
Tristan <tristanskyhook@gmail.com>
2023-08-26 12:01:38 -0700

    dz

---
+++ src/cube.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+use bevy_mod_picking::prelude::*;
+
+// PbrBundle {
+//     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+//     material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+//     transform: Transform::from_xyz(1.5, 0.5, 1.5),
+//     ..default()
+// });
+//cube extends PbrBundle with a pickable component
